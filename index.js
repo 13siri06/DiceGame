65882ce69d11cd357054c55764cff10e80c051f3
13siri06 <13siri06@gmail.com>
2026-05-26 14:23:27 +0530

    Upgraded the game from refersh to button click

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
+document.querySelector("button").addEventListener("click", function () {
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  // 🎲 Player 1 - Generate random dice number and update image
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  // 🎲 Player 2 - Generate random dice number and update image
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+  // 🏆 Winner Check
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "⚖️ Draw!";
+  }
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+});
